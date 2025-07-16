058424faa03cb7fd9c6d80603153fe73d234b6af
Rubén Hernández Saénz <rubenhernandezsaenz@MacBook-Air-de-Ruben.local>
2025-07-16 15:55:46 -0600

    Proyectoinicial: React + Vite + Tailwind

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+base: '/', // Asegúrate de esto también
+build: {
+outDir: 'dist'
+,
+},
+server: {
+historyApiFallback: true,
+}
+});
