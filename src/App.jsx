9d7a8731a72ee42330db904155530b8027e3bd3c
Emiliano Hernadez Saenz <emilianohernadezsaenz@MacBook-Air-de-Emiliano.local>
2025-08-02 13:43:32 -0600

    tarea del 29 julio

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 
-/*Arrow Function sin parámetros (10 pts)
-Crea una función flecha que no reciba ningún parámetro y que, al llamarse, imprima en consola un saludo como "¡Bienvenido al sistema!".
-*/
+function App() {
+  /* 1. Arrow Function sin parámetros (10 pts) 
+  Crea una función flecha que no reciba ningún parámetro y que, al llamarse, imprima en consola un saludo como "¡Bienvenido al sistema!".
+  */
+  const saludo = () => {
+    console.log("bienvenido al sistema");
+  }
+  
 
-const saludar = () => {
-  console.log("¡Bienvenido al sistema!");
-}
 
-/*Arrow Function con parámetros (10 pts)
-Crea una función flecha que reciba un nombre como parámetro y muestre en consola:
+  /* 2. Arrow Function con parámetros
+  Crea una función flecha que reciba un nombre como parámetro y muestre en consola:
 "Hola, me llamo [nombre]".
 */
-const saludarNombre = (nombre) => {
-  console.log(`Hola, me llamo ${nombre}`);
-}
+  const parametro = (nombre) => {
+    console.log("hola me llamo, ${nombre}");
+  }
+  
 
-/*Arrow Function con parámetros y retorno (10 pts)
-Crea una función flecha que reciba dos números y retorne su suma.
+
+  /*3. Arrow Function con parámetros y retorno
+  Crea una función flecha que reciba dos números y retorne su suma.
 Luego muestra el resultado en consola.
 */
+  const sumar = (a,b) => a+b;
+  const resultado = sumar(18,90);
+  console.log("la suma es: + {resultado}");
 
-const sumar = (num_1, num_2) => {
-  return num_1 + num_2;
-}
-
-/*Evaluador con if, else if y else (15 pts)
-Crea una función flecha llamada evaluarNota que reciba una calificación numérica (0 a 10) y muestre:
+  /*4. Evaluador con if, else if y else
+  crea una función flecha llamada evaluarNota que reciba una calificación numérica (0 a 10) y muestre:
 
 "Excelente" si es 10
 
@@ -35,38 +38,37 @@ Crea una función flecha llamada evaluarNota que reciba una calificación numér
 
 "Reprobado" si es menor a 6
 */
-const evaluarNota = (calificacion) => {
-  if (calificacion === 10) {
-    console.log("Excelente");
+  const evaluarNota = (calificacion) => {
+    if (calificacion === 10) {
+      console.log("exelente");
+    } else if(calificacion === 9 && calificacion === 8) {
+      console.log("muy bien");
+    } else if(calificacion === 7 && calificacion === 6) {
+      console.log("bien");
+    } else if(calificacion <=6) {
+      console.log("reprobado");
+    }
   }
-  else if (calificacion >= 8 && calificacion <= 9) {
-    console.log("Muy bien");
-  }
-  else if (calificacion >= 6 && calificacion < 8) {
-    console.log("Bien");
-  }
-  else {
-    console.log("Reprobado");
-  }
-}
-/*Condicional con operador lógico y comparadores (15 pts)
-Crea una función flecha llamada verificarAcceso que reciba dos datos: edad y si tiene ticket (true o false).
+  
+
+  /*5.  Condicional con operador lógico y comparadores 
+  Crea una función flecha llamada verificarAcceso que reciba dos datos: edad y si tiene ticket (true o false).
 Debe mostrar:
 
 "Acceso permitido" si tiene 18 años o más y tiene ticket.
 
 "Acceso denegado" en cualquier otro caso.
-*/
-
-const verificarAcceso = (edad, tieneticket = true) => {
-  if (edad >= 18 && tieneticket) {
-    console.log("Acceso permitido");
+  */
+  const verificarAcceso = (edad, tieneTicket) => {
+    if(edad >=18 && tieneTicket) {
+      console.log("acceso permitido");
   } else {
-    console.log("Acceso denegado");
+    console.log("acceso denegado");
   }
 }
 
-/*Arrow function con if anidados y else if (20 pts)
+
+/*6. Arrow function con if anidados y else if 
 Crea una función flecha llamada clasificarUsuario que reciba tres datos: edad, si tiene INE (true o false), y si tiene invitación (true o false).
 
 Lógica:
@@ -85,26 +87,22 @@ Si tiene INE o tiene invitación → "Acceso completo autorizado"
 
 Si no tiene ninguno → "No puedes ingresar"
 */
-
-const clasificarUsuario = (edad, tieneINE = false, tieneInvitacion = false) => {
-  if (edad < 18) {
-    console.log("Acceso denegado por edad");
-  } else if (edad >= 18 && edad < 21) {
-    if (tieneINE) {
-      console.log("Acceso limitado");
-    } else {
-      console.log("Debes presentar identificación");
-    }
-  } else if (edad >= 21) {
-    if (tieneINE || tieneInvitacion) {
-      console.log("Acceso completo autorizado");
-    } else {
-      console.log("No puedes ingresar");
-    }
-  }
+const clasificarUsuario = (edad , INE , invitacion) => {
+if(edad <18){
+  console.log("acceso denegado por edad");
+} else if (edad>=18 && edad <21 && INE){
+  console.log("acceso limitado")
+} else if (edad>=18 && edad <21 && !INE){
+console.log("debes presentar indentificacion")}
+ if(edad>=21 || INE || invitacion){
+  console.log("acceso completo autorizado");
+} else {
+  console.log("no puedes ingresar")
+}
 }
 
-/*Evaluador avanzado con varias funciones (20 pts)
+
+/*7.Evaluador avanzado con varias funciones 
 Crea las siguientes funciones flecha:
 
 esPar(numero) → retorna true si el número es par
@@ -121,14 +119,30 @@ evaluarEntrada(edad, tieneTicket, esInvitado) → muestra:
 
 Llama a las 3 funciones con valores de prueba y muestra los resultados en consola.
 */
+const esPar = {} //no sabia como hacerlo
 
 
 
-const App = () => {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-purple-600">¡Que rollo mi Carlos?!</h1>
-    </div>
-  );
+
+const clasificarEdad = (edad) => {
+  if(edad <13){
+    return "nino";
+  } else if(edad >13 && edad <18) {
+    return "adolescente";
+  } else {
+  return "adulto";
+  }
+}
+
+const evaluarEntrada =(edad, tieneTicket,esInvitado) => {
+  if(edad <18 && esInvitado) {
+    console.log("si es menor de edad pero esta invitado");
+  } else if(edad>=18 && tieneTicket) {
+    console.log("si tiene mas de 18 y tiene ticket");
+  } else {
+    console.log("acceso denegado");
+  }
+}
+  
 }
 export default App;
